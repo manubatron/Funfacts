9b32d626caea5186183c6c9424645182fb6a990c
Julius Manubay <juliusmanubay@juliuss-macbook-pro.local>
2017-10-18 20:38:20 -0600

    Initial Commit

---
+++ Funfacts/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Funfacts
+//
+//  Created by Julius Manubay on 10/18/17.
+//  Copyright © 2017 Manubatron Solutions. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
